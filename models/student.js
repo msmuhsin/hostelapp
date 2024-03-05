58a677e0be68fad3fafdc555cb0542837675c04a
Abdul Majid <majid616365@gmail.com>
2024-03-05 21:34:19 +0530

    update

---
+++ models/student.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+});
+
+const Student = mongoose.model('Student', studentSchema);
