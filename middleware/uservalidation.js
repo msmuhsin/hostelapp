e49d473f8aad28998fd94693107d47599aa1b847
Abdul Majid <majid616365@gmail.com>
2024-05-29 21:18:03 +0530

    update validation

--- middleware/uservalidation.js
+++ middleware/uservalidation.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 export const userValidate = async (req, res, next) => {
-  const token = req.header('Authorization')
+  const token = req.headers.authorization
 
   if (!token) {
     return res.status(401).send({ message: 'Access Denied', success: false })
